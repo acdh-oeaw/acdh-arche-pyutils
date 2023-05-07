16cd8092c8f7c367784d144d84c9c91848238a55
csae8092 <peter.andorfer@oeaw.ac.at>
2023-05-07 15:00:05 +0200

    fix for setup.py

--- setup.py
+++ setup.py
@@ -23,44 +23,41 @@ def walker(base, *paths):
     return list(file_list)
 
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
     author="Peter Andorfer",
-    author_email='peter.andorfer@oeaw.ac.at',
-    python_requires='>=3.7',
+    author_email="peter.andorfer@oeaw.ac.at",
+    python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="a python client for ARCHE-API",
     entry_points={
-        'console_scripts': [],
+        "console_scripts": [],
     },
     install_requires=requirements,
     license="MIT",
     long_description=readme,
-    long_description_content_type="text/markdown"
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    keywords='acdh-arche-pyutils',
-    name='acdh-arche-pyutils',
-    packages=find_packages(include=['acdh_arche_pyutils', 'acdh_arche_pyutils.*']),
+    keywords="acdh-arche-pyutils",
+    name="acdh-arche-pyutils",
+    packages=find_packages(include=["acdh_arche_pyutils", "acdh_arche_pyutils.*"]),
     package_data={
-        module.__name__: walker(
-            os.path.dirname(module.__file__),
-            'files'
-        ),
+        module.__name__: walker(os.path.dirname(module.__file__), "files"),
     },
-    url='https://github.com/acdh-oeaw/acdh-arche-pyutils',
-    version='0.7.1',
+    url="https://github.com/acdh-oeaw/acdh-arche-pyutils",
+    version="0.7.1",
     zip_safe=False,
 )
